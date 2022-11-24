e23abb9f6e7731eaf95d4c63a6c827abfa626ad9
Nathan Pigeyre <nathan.pigeyre@uha.fr>
2022-11-24 09:13:35 +0100

    Initial commit

---
+++ TP2ex1.1.py
@@ -0,0 +1,7 @@
+x = 1
+y = 2
+print("Avant permutation:\nEntrez x: {} \nEntrez y: {} ".format(x , y))
+m = x
+x = y
+y = m
+print("Après permutation:\nEntrez x: {} \nEntrez y: {} ".format( x , y))
---
+++ TP2ex2.py
@@ -0,0 +1,5 @@
+print('Donnez votre age :')
+age = int(input())
+annee = 2022 - age
+print('Votre annee de naissance est :' , annee)
+
